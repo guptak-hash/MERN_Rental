01ae61a1726251bce60b603512b7fe9748d84689
guptak-hash <guptakishan1123@gmail.com>
2025-07-02 14:02:41 +0530

    changed base url

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { toast } from 'react-hot-toast';
 import { useNavigate } from "react-router-dom";
 
-axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.baseURL = 'https://mern-rental-backend.vercel.app'
 
 export const AppContext = createContext();
 
